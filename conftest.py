cad93bd034ec17b1096ded7c19c89b6025cbb9d3
l <lla@ls-MacBook-Pro.local>
2026-06-30 18:02:38 +0800

    fix: add headless mode for CI

--- conftest.py
+++ conftest.py
@@ -5,6 +5,10 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     options = Options()
+    options.add_argument("--headless")  # 无头模式
+    options.add_argument("--nosandbox") #ci环境必须
+    options.add_argument("--disable-dev-shm-usage") #ci环境必须
+    options.add_argument("--window-size=1920,1080") #设置窗口大小
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(1)
     driver.maximize_window()
